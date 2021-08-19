1551151d4b553b6606559cb0395c03ef5113a434
Someguy1834 <TheAwesomer1452@gmail.com>
2021-08-19 16:06:02 -0400

    Fixed indent issue

--- src/assembly-generator.cpp
+++ src/assembly-generator.cpp
@@ -1,6 +1,6 @@
 std::string addition(const Branch &branch)
 {
-    std::string out = "\tadd\tebx, ";
+    std::string out = "\t\tadd\t\tebx, ";
     out += std::to_string(branch.value2);
     out += "\n";
     return out;
@@ -8,7 +8,8 @@ std::string addition(const Branch &branch)
 
 std::string subtract(const Branch &branch)
 {
-    std::string out = "\tsub\tebx, ";
+    std::string out = "\t\tsub"
+                      "\t\tebx, ";
     out += std::to_string(branch.value2);
     out += "\n";
     return out;
@@ -16,29 +17,29 @@ std::string subtract(const Branch &branch)
 
 std::string multiply(const Branch &branch)
 {
-    std::string out = "\tmov\teax, ";
+    std::string out = "\t\tmov\t\teax, ";
     out += std::to_string(branch.value2);
     out += "\n"
-           "\tmul\tebx\n"
-           "\tmov\tebx, eax\n";
+           "\t\tmul\t\tebx\n"
+           "\t\tmov\t\tebx, eax\n";
     return out;
 }
 
 std::string divide(const Branch &branch)
 {
-    std::string out = "\tmov\tedx, 0\n"
-                      "\tmov\teax, ebx\n"
-                      "\tmov\tecx, ";
+    std::string out = "\t\tmov\t\tedx, 0\n"
+                      "\t\tmov\t\teax, ebx\n"
+                      "\t\tmov\t\tecx, ";
     out += std::to_string(branch.value2);
     out += "\n"
-           "\tdiv\tecx\n"
-           "\tmov\tebx, eax\n";
+           "\t\tdiv\t\tecx\n"
+           "\t\tmov\t\tebx, eax\n";
     return out;
 }
 
 std::string init(const Branch &branch)
 {
-    std::string out = "\tmov\tebx, ";
+    std::string out = "\t\tmov\t\tebx, ";
     out += std::to_string(branch.value1);
     out += "\n";
     return out;
